5f097dc4134b0ec08b331e3425025b134b3d702a
KrishaShah <krishabr2007@gmail.com>
2026-07-31 18:20:47 +0530

    portal_backend

---
+++ portal_backend/src/test/java/com/nirma/portal/portal_backend/PortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirma.portal.portal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
